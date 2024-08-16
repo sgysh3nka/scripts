153b252a5e13be3ca9c1af8bf767894df42926b2
Dmitry <dimonmini663@gmail.com>
2024-08-16 13:19:01 +0300

    Update sgysh3nkahub.lua

--- PrisonLife/sgysh3nkahub.lua
+++ PrisonLife/sgysh3nkahub.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-    local Window = Library.CreateLib("Sgysh3nkaHUB x Prison Life", "Midnight")
+    local Window = Library.CreateLib("Sgysh3nkaHUB x Prison Life", "BloodTheme")
  
     -- MAIN
     local Main = Window:NewTab("Main")
