9ec50128023a6544554cde77f9505c68ff704797
Dmitry <dimonmini663@gmail.com>
2024-08-16 18:55:43 +0300

    Update sgysh3nkahub.lua

--- MM2/sgysh3nkahub.lua
+++ MM2/sgysh3nkahub.lua
@@ -1,8 +1,12 @@
 -- ONLY PC
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("sgysh3nkaHUB x MM2", "BloodTheme")
+
+
+-- Player
 local Tab = Window:NewTab("Player")
 local Section = Tab:NewSection("SuperMan")
+
 Section:NewToggle("SpeedHack", "You can be fast!", function(state)
     if state then
         game.Players.localPlayer.Character.Humanoid.WalkSpeed = 40
@@ -10,6 +14,7 @@ Section:NewToggle("SpeedHack", "You can be fast!", function(state)
         game.Players.localPlayer.Character.Humanoid.WalkSpeed = 25
     end
 end)
+
 Section:NewToggle("JumpHack", "Same with SpeedHack but jump power", function(state)
     if state then
         game.Players.localPlayer.Character.Humanoid.JumpPower = 100
@@ -18,17 +23,23 @@ Section:NewToggle("JumpHack", "Same with SpeedHack but jump power", function(sta
     end
 end)
 
+
+-- Local
 local Tab = Window:NewTab("Local")
 local Section = Tab:NewSection("i caca")
 Section:NewButton("Esp", "You can view players thru walls!", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/sgysh3nka/scripts/main/MM2/mm2espscript.lua"))()
 end)
 
+
+-- Other
 local Tab = Window:NewTab("Other")
 local Section = Tab:NewSection("Something what you need")
+
 Section:NewButton("TP Tool", "Teleports you!", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/sgysh3nka/scripts/main/UNIVERSAL/tptool.lua"))()
 end)
+
 Section:NewButton("Fly", "You can fly (yay)", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/sgysh3nka/scripts/main/UNIVERSAL/flyscript.lua"))()
 end)
